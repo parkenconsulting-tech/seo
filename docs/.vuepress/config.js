ea5bd050bacf6819cd0ca8c2c9a21c00c6d5ff73
parkenconsulting-tech <59141141+parkenconsulting-tech@users.noreply.github.com>
2020-01-02 08:09:50 +0530

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  title: "Vuepress",
-  description: "This is a Zeit Now 2.0 example",
+  title: "Parken Consulting - SEO Docs",
+  description: "Search Engine Optimization with Laravel, SEMRush & Algolia",
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
-      { text: "Guide", link: "/guide/" },
-      { text: "Config Page", link: "/config" }
+      { text: "Google Compat", link: "/guide/" },
+      { text: "Performance", link: "/config" },
+      { text: "Engagement", link: "/config" },
+      { text: "Fullfilment", link: "/config" },
     ]
   },
   dest: "public"
